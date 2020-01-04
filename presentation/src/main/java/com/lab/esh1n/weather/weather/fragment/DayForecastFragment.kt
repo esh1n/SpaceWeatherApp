c7b2b9f44a5ccf7de9134d6c789551225c9924b4
Sergey Eshin <sergey.eshin@dataart.com>
2020-01-04 15:32:19 +0300

    added day forecast screen

--- presentation/src/main/java/com/lab/esh1n/weather/weather/fragment/DayForecastFragment.kt
+++ presentation/src/main/java/com/lab/esh1n/weather/weather/fragment/DayForecastFragment.kt
@@ -1,32 +1,31 @@
 package com.lab.esh1n.weather.weather.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.lab.esh1n.weather.R
+import com.lab.esh1n.weather.weather.model.ForecastDayModel
 import kotlinx.android.synthetic.main.layout_position.*
 
-const val ARG_TITLE = "ARG_TITLE"
 
 /**
  * Fragment to be displayed in [FragmentAdapter]
  *
  * Contains item position and displays it in TextView
  */
-class DayForecastFragment : Fragment() {
+class DayForecastFragment : Fragment(R.layout.layout_position) {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.layout_position, container, false)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val position = arguments?.getString(ARG_TITLE) ?: ""
-        showPosition(position)
+        val dayModel = arguments?.getParcelable<ForecastDayModel>(ARG_PLACE_DAY_FORECAST)
+        showTitle(dayModel?.dayDescription ?: "")
     }
 
-    private fun showPosition(description: String) {
+    private fun showTitle(description: String) {
         name_tv.text = description
+    }
+
+    companion object {
+        const val ARG_PLACE_DAY_FORECAST = "ARG_PLACE_DAY_FORECAST"
     }
 }
